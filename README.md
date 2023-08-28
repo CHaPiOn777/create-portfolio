1cca8f9ec85a7826df524e35a69f341dabf6d1e5
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2023-08-28 19:48:59 +0300

    доделал проект

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # create-portfolio
+Этот проект для отработки навыков с анимированным скроллингом. Сверстан небольшой лендинг-портфолио по готовому дизайну
+
+# Технологии в проекте:
+HTML5  
+CSS3  
+gsap  
+ScrollTrigger  
+ScrollSmoother
+
+# [Ссылка на сайт](https://chapion777.github.io/create-portfolio/)
--- js/app.js
+++ js/app.js
@@ -6,4 +6,46 @@ ScrollSmoother.create({
   smooth: 1.5,
   effects: true,
   smoothTouch: 0.1,
+});
+
+gsap.fromTo('.header-section', { opacity: 1, transition: 'opacity 1s' }, {
+  opacity: 0,
+  transition: 'opacity .5s',
+  scrollTrigger: {
+    trigger: '.header-section',
+    start: 'center',
+    end: '70vh',
+
+    scrub: true,
+  }
+})
+
+let itemsL = gsap.utils.toArray('.left-column .gallery__item');
+
+itemsL.forEach(item => {
+  gsap.fromTo(item, { x: -150, opacity: 0 }, {
+    opacity: 1,
+    x: 0,
+    scrollTrigger: {
+      trigger: item,
+      start: '-1000',
+      end: '-100',
+      scrub: true
+    }
+  })
+})
+
+let itemsR = gsap.utils.toArray('.right-column .gallery__item');
+
+itemsR.forEach(item => {
+  gsap.fromTo(item, { x: 100, opacity: 0 }, {
+    opacity: 1,
+    x: 0,
+    scrollTrigger: {
+      trigger: item,
+      start: '-1000',
+      end: '-100',
+      scrub: true
+    }
+  })
 })
